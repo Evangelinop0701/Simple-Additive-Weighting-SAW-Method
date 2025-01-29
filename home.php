3932874cff7f9403d5d5785e0824eeb1999378ba
Your Name <evange.21028@gmail.com>
2025-01-29 11:43:16 +0900

    Aumenta textu iha contex iha pagina home nian

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="text-center mt-5">
-        <h1>Sistema Apoiu Desisaun</h1>
-        <p class="lead">Metodu SAW</p>
+        <h1>Sistema Apoiu Desisaun Utiliza</h1>
+        <p class="lead">Metodu SAW & Linguagen Programasaun PHP</p>
         <p><i>Develop by: Evangelino Soares Saldanha</i></p>
     </div>
 </div>
